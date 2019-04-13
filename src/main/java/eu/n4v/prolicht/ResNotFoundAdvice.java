a633f48737768a01b0319faeb2b87b10d1c83b7f
Markus Kolb <makolb@n4v.eu>
2019-04-13 21:12:37 +0200

    feat: make ResNotFoundException public

--- src/main/java/eu/n4v/prolicht/ResNotFoundAdvice.java
+++ src/main/java/eu/n4v/prolicht/ResNotFoundAdvice.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-class ResNotFoundAdvice {
+public class ResNotFoundAdvice {
 
     @ResponseBody
     @ExceptionHandler(ResNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String resNotFoundHandler(ResNotFoundException ex) {
+    public String resNotFoundHandler(ResNotFoundException ex) {
         return ex.getMessage();
     }
 }
